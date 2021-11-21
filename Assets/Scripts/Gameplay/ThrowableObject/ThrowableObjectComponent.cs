f08109185d6626eee613f94f8295e177d3ba0cbb
Sorien <sorien787@gmail.com>
2021-11-21 19:35:17 +0100

    Make throwable objects pause

--- Assets/Scripts/Gameplay/ThrowableObject/ThrowableObjectComponent.cs
+++ Assets/Scripts/Gameplay/ThrowableObject/ThrowableObjectComponent.cs
@@ -2,7 +2,7 @@
 using System;
 
 [RequireComponent(typeof(FreeFallTrajectoryComponent))]
-public class ThrowableObjectComponent : IThrowableObjectComponent, IHealthListener
+public class ThrowableObjectComponent : IThrowableObjectComponent, IHealthListener, IPauseListener
 {
     [SerializeField] private Rigidbody m_ThrowingBody;
 
@@ -16,6 +16,8 @@ public class ThrowableObjectComponent : IThrowableObjectComponent, IHealthListen
 
 	[SerializeField] protected FreeFallTrajectoryComponent m_FreeFallComponent;
 
+	[SerializeField] private CowGameManager m_Manager;
+
 	public event Action OnDestroyed;
 
 	public bool IsImmediatelyThrowable { get; set; } = false;
@@ -25,15 +27,6 @@ public class ThrowableObjectComponent : IThrowableObjectComponent, IHealthListen
 	public override Transform GetAttachmentTransform => m_AttachmentTransform;
 
 	public override Transform GetMainTransform => m_MainTransform;
-
-	public void Awake()
-	{
-		m_FreeFallComponent.OnObjectNotInFreeFall += OnObjectLanded;
-		if (TryGetComponent(out HealthComponent healthComponent))
-		{
-			healthComponent.AddListener(this);
-		}
-	}
 
 	public override void ThrowObject(in ProjectileParams pParams)
 	{
@@ -59,5 +52,43 @@ public class ThrowableObjectComponent : IThrowableObjectComponent, IHealthListen
 	public void OnEntityDied(GameObject go1, GameObject go2, DamageType type)
 	{
 		OnDestroyed?.Invoke();
+	}
+
+	public void Awake()
+	{
+		m_Manager.AddToPauseUnpause(this);
+		m_FreeFallComponent.OnObjectNotInFreeFall += OnObjectLanded;
+		if (TryGetComponent(out HealthComponent healthComponent))
+		{
+			healthComponent.AddListener(this);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		m_Manager.RemoveFromPauseUnpause(this);
+	}
+
+	Vector3 m_cachedVelocity = Vector3.zero;
+	bool m_bHadVelocity = false;
+
+	public void Pause()
+	{
+		if (!m_ThrowingBody.isKinematic)
+		{
+			m_bHadVelocity = true;
+			m_cachedVelocity = m_ThrowingBody.velocity;
+			m_ThrowingBody.isKinematic = true;
+		}
+	}
+
+	public void Unpause()
+	{
+		if (m_bHadVelocity) 
+		{
+			m_bHadVelocity = false;
+			m_ThrowingBody.velocity = m_cachedVelocity;
+			m_ThrowingBody.isKinematic = false;
+		}
 	}
 }
